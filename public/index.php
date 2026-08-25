b7118e45676571ce370f5e4242d92df88bfea967
joao assis <jg.morais09@gmail.com>
2026-08-25 13:51:34 -0300

    Criando o projeto

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="title">Sign in to TaskCore</h1>
+    <div div="input-block">
+    </div>
+    <div div="have-account-block"></div>
+</body>
+</html>
