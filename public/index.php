a6b579b1fd4844cb45c6314171b790b0e92f4b60
BlueCatCom <262645688+bluecatcom@users.noreply.github.com>
2026-08-25 18:12:03 +0000

    Autenticar registro

--- public/index.php
+++ public/index.php
@@ -3,25 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign in TaskCore</title>
+    <title>Sign up TaskCore</title>
     <link rel="stylesheet" href="css/sign-up.css">
 </head>
 <body>
     <img id="logo" src="css/images/taskcore-logo.svg" alt="TaskCore Logo">
 
     <h1 class="title">Sign up to TaskCore</h1>
+    
+    <form method="POST" action="autenticarRegistro.php">
+
     <div id="input-block">
         <label for="username">Username</label>
-        <input id="username" placeholder="Noobmaster69"><br>
+        <input id="username" name="nome" placeholder="Noobmaster69" required><br>
 
         <label for="email">E-mail</label>
-        <input id="email" placeholder="your@email.com" type="email"><br>
+        <input id="email" name="email" placeholder="your@email.com" type="email" required><br>
 
         <label for="password">Password</label>
-        <input id="password" placeholder="password123" type="password"><br>
+        <input id="password" name="password" placeholder="password123" type="password" required><br>
 
         <button id="button-sign-up" type="submit">Sign up</button>
     </div>
+    
+    </form>
+    
     <div id="have-account-block">
         <h1>Already have an Account? </h1>
         <a href="sign-in.php">Login</a>
